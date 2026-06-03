e070becd20c3ec28ec08197a0a81877ee5cc3e74
koushik-dasss <koushikdasmagnate2005@gmail.com>
2026-06-04 00:06:02 +0530

    Confusion matrix from actual and prediction values using TP,FP,TN.FN

---
+++ Assignment-3/Q2.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<Eigen/Dense>
+using namespace std;
+using namespace Eigen;
+int main(){
+    VectorXi actual(10);
+    actual<< 1,0,1,1,0,1,0,0,1,0;
+    VectorXi predicted(10);
+    predicted<< 1,0,1,0,0,1,0,1,1,0;
+    int TP = 0 , TN = 0 , FP = 0 , FN = 0;
+    for(int i=0 ; i<10 ; i++){
+        if(actual(i) == 1 && predicted(i) == 1){
+            TP++;
+        }else if(actual(i) == 0 && predicted(i) == 0){
+            TN++;
+        }else if(actual(i) == 0 && predicted(i) == 1){
+            FP++;
+        }else{
+            FN++;
+        }
+    }
+    double accuracy = (TP+TN)/10.0;
+    double precision = TP/double(TP+FP);
+    double recall = TP/double(TP+FN);
+    double fscore = 2*precision*recall/(precision+recall);
+    cout<<"TP = "<<TP<<endl;
+    cout<<"TN = "<<TN<<endl;
+    cout<<"FP = "<<FP<<endl;
+    cout<<"FN = "<<FN<<endl;
+    cout<<"Accuracy = "<<accuracy<<endl;
+    cout<<"Precision = "<<precision<<endl;
+    cout<<"Recall = "<<recall<<endl;
+    cout<<"F1-Score = "<<fscore<<endl;
+    return 0;
+}
