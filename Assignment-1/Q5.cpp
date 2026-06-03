0b0accd2015575bc8f3c98fe631512c4f2605589
koushik-dasss <koushikdasmagnate2005@gmail.com>
2026-06-03 21:27:36 +0530

    Mathematical operations on a matrix

--- Assignment-1/Q5.cpp
+++ Assignment-1/Q5.cpp
@@ -15,3 +15,24 @@ int main(){
     cout<<"Matrix Division: "<<endl<<A*B.inverse()<<endl;
     return 0;
 }
+// Output:- 
+// g++ -I /usr/include/eigen3 Q5.cpp -o Q5 
+// ./Q5
+// Matrix A: 
+// 1 2
+// 3 4
+// Matrix B: 
+// 5 6
+// 7 8
+// Matrix Addition: 
+//  6  8
+// 10 12
+// Matrix Subtraction: 
+// -4 -4
+// -4 -4
+// Matrix Multiplication: 
+// 19 22
+// 43 50
+// Matrix Division: 
+//  3 -2
+//  2 -1
